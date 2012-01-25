6e9900b8e3b76588276b00d5887c04ed7cd27201
yonathan0210 <yonathan0210@29bf9467-4a12-4297-bab6-064b40403d6e>
2012-01-25 02:21:30 +0000

    updated runTest script to include affinities Added sh script for tests with even numbers of producers and consumers with affinity assignment

--- SCPools/scripts/runTest.pl
+++ SCPools/scripts/runTest.pl
@@ -3,37 +3,54 @@
 use Getopt::Long;
 
 my $force="no";
-my $cons = 1;
-my $prod = 1;
+my $consNum = 1;
+my $prodNum = 1;
 my $poolType = "NoFIFOPool";
-my $fileName = "SCPools";
+my $fileName = "SCPools-24-01";
 my $prodFluct="false";
 my $consFluct="false";
 my $pausedThreads=0;
 my $prodMigrate="true";
+my $prodCores="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16";
+my $consCores="17 18 19 20 21 22 23 24 25 26 27 28 29 30 31";
+my $chips="0 8 16 24:1 9 17 25:2 10 18 26:3 11 19 27:4 12 20 28:5 13 21 29:6 14 22 30:7 15 23 31";
+my $affinity_str="x";
+my @affinity;
 
-GetOptions( "consumers=i" => \$cons,
-	    "producers=i" => \$prod,
+GetOptions( "consumers=i" => \$consNum,
+	    "producers=i" => \$prodNum,
 	    "poolType=s"  => \$poolType,
 	    "file=s"  => \$fileName,
 	    "force=s" => \$force,
 		"prodFluct=s" => \$prodFluct,
 		"consFluct=s" => \$consFluct,
 		"pausedThreads=i" => \$pausedThreads,
-		"prodMigrate=s" => \$prodMigrate
+		"prodMigrate=s" => \$prodMigrate,
+		"affinity=s" => \$affinity_str
 		);
+		
+if($affinity_str ne "x")
+{
+	@affinity = split(/\*/,$affinity_str);
+	$prodCores = $affinity[0];
+	$consCores = $affinity[1];
+	$chips = $affinity[2];
+	$prodCores =~ s/_/ /g;
+	$consCores =~ s/_/ /g;
+	$chips =~ s/_/ /g;
+}
 
 $conf = <<END;
 #force affinity ?
-force=$force
+forceAssignment=$force
 #if forced affinity - producers/comsumers thread cores
-cons=0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-prods=16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
-chips=0 8 16 24:1 9 17 25:2 10 18 26:3 11 19 27:4 12 20 28:5 13 21 29:6 14 22 30:7 15 23 31
+prods=$prodCores
+cons=$consCores
+chips=$chips
 
 #number of producer/consumer threads
-producersNum=$prod
-consumersNum=$cons
+producersNum=$prodNum
+consumersNum=$consNum
 
 #type of container
 poolType=$poolType
