a6a96ce95c148bfa667a1f58ca5e82dbf14b8c25
yonathan0210 <yonathan0210@29bf9467-4a12-4297-bab6-064b40403d6e>
2012-01-27 13:59:29 +0000

    corrected a small bug in testbench_asymmetric.sh

--- SCPools/scripts/testbench_asymmetric.sh
+++ SCPools/scripts/testbench_asymmetric.sh
@@ -16,13 +16,13 @@ for poolType in "NoFIFOCASPool" "LIFOPool" "NoFIFOPool"  "MSQTaskPool"; do
 				for aff2 in "0_1_2_3_8_9_10_11*4_5_6_7_12_13_14_15_16_17_18_19_20_21_22_23_24_25_26_27_28_29_30_31*0_8_16_24:1_9_17_25:2_10_18_26:3_11_19_27:4_12_20_28:5_13_21_29:6_14_22_30:7_15_23_31"; do
 				echo "Affinity 2"
 				for i in `seq 5`; do 
-					perl runTest.pl --consumers=$c --producers=$p --poolType=$poolType --force="yes" --prodMigrate=$prodMigrate --affinity=$aff2 --affinityIdx="6" >> res_assymetric.csv
+					perl runTest.pl --consumers=$c --producers=$p --poolType=$poolType --force="yes" --prodMigrate=$prodMigrate --affinity=$aff2 --affinityIdx="6" >> res_asymmetric.csv
 				done
 				done
 				
 				echo "no affinity"
 				for i in `seq 5`; do 
-					perl runTest.pl --consumers=$c --producers=$p --poolType=$poolType --force="no" --prodMigrate=$prodMigrate --affinityIdx="0" >> res_assymetric.csv
+					perl runTest.pl --consumers=$c --producers=$p --poolType=$poolType --force="no" --prodMigrate=$prodMigrate --affinityIdx="0" >> res_asymmetric.csv
 				done
 			done
 		done
@@ -36,20 +36,20 @@ for poolType in "NoFIFOCASPool" "LIFOPool" "NoFIFOPool"  "MSQTaskPool"; do
 				for aff3 in "8_9_10_11_12_13_14_15_16_17_18_19_20_21_22_23_24_25_26_27_28_29_30_31*0_1_2_3_4_5_6_7*0_8_16_24:1_9_17_25:2_10_18_26:3_11_19_27:4_12_20_28:5_13_21_29:6_14_22_30:7_15_23_31"; do
 				echo "Affinity 3"
 				for i in `seq 5`; do 
-					perl runTest.pl --consumers=$c --producers=$p --poolType=$poolType --force="yes" --prodMigrate=$prodMigrate --affinity=$aff3 --affinityIdx="7" >> res_assymetric.csv
+					perl runTest.pl --consumers=$c --producers=$p --poolType=$poolType --force="yes" --prodMigrate=$prodMigrate --affinity=$aff3 --affinityIdx="7" >> res_asymmetric.csv
 				done
 				done
 		
 				for aff4 in "4_5_6_7_12_13_14_15_16_17_18_19_20_21_22_23_24_25_26_27_28_29_30_31*0_1_2_3_8_9_10_11*0_8_16_24:1_9_17_25:2_10_18_26:3_11_19_27:4_12_20_28:5_13_21_29:6_14_22_30:7_15_23_31"; do
 				echo "Affinity 4"
 				for i in `seq 5`; do 
-					perl runTest.pl --consumers=$c --producers=$p --poolType=$poolType --force="yes" --prodMigrate=$prodMigrate --affinity=$aff4 --affinityIdx="8" >> res_assymetric.csv
+					perl runTest.pl --consumers=$c --producers=$p --poolType=$poolType --force="yes" --prodMigrate=$prodMigrate --affinity=$aff4 --affinityIdx="8" >> res_asymmetric.csv
 				done
 				done
 		
 				echo "no affinity"
 				for i in `seq 5`; do 
-					perl runTest.pl --consumers=$c --producers=$p --poolType=$poolType --force="no" --prodMigrate=$prodMigrate --affinityIdx="0"  >> res_assymetric.csv
+					perl runTest.pl --consumers=$c --producers=$p --poolType=$poolType --force="no" --prodMigrate=$prodMigrate --affinityIdx="0"  >> res_asymmetric.csv
 				done
 			done 
 		done
