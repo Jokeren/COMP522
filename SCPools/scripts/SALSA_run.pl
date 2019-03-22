4272efc0016bef9333f854135540e17f1f8973c1
Jokeren <robinho364@gmail.com>
2019-03-22 12:30:45 -0500

    Remove unrelated files

--- SCPools/scripts/SALSA_run.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-foreach $dir (@ARGV) {
-	$outDir = $dir;
-	$outDir =~ s/input/output/;
-	$iterations = 5;
-	system("mkdir -p $outDir");
-	system("perl SALSA_test.pl $dir $outDir $iterations");
-	system("perl SALSA_csv.pl $outDir $iterations");
-}
